47c34af569ba3b48bf1adb8a748e83f5d53150ac
kushalverma02 <verma.kushal9287@gmail.com>
2025-04-23 17:07:29 +0530

    adding admin url

--- recipefinder/urls.py
+++ recipefinder/urls.py
@@ -21,6 +21,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+     path('admin/', admin.site.urls),
      path('' , views.about,name = 'about'),
     path('base/' , views.base,name = 'base'),
     path('login/' , views.user_login,name = 'login'),
